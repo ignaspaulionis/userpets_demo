94d244e9766c86951369a1e9e2c77203144f0cb8
Ignas Paulionis <ignaspaulionis@gmail.com>
2026-02-24 09:31:39 +0200

    SCRUM-20: Add GET /api/stats endpoint

---
+++ routes/stats.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const User = require('../models/user');
+const { Pet } = require('../models/pet');
+const { authMiddleware } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/stats', authMiddleware, async (req, res) => {
+  try {
+    const [users, pets] = await Promise.all([User.count(), Pet.count()]);
+
+    return res.status(200).json({ users, pets });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
